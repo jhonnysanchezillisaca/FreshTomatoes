28325aefbea701f35eab03806b30e6ecf2df7f30
jhonnysanchezillisaca <jhonny15993@gmail.com>
2016-10-10 09:54:05 +0200

    Added TMDB API for trailers
    TODO clean code

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,20 +4,56 @@ import fresh_tomatoes
 import requests
 
 
-urlApi = "https://www.omdbapi.com/?%s"
+def getTrailer(tmdb_id):
+    print "\n\n\nIn getTrailer()"
+    url_rq = urlTMDBTrailer % (tmdb_id)
+    rq_trailer = requests.get(url_rq, params_TMDB_trailer)
+    dataTMDB = json.loads(rq_trailer.content)
+    print rq_trailer.url
+    print dataTMDB
+    return urlYoutube % dataTMDB['results'][0]['key']
 
+
+def getTMDBID(imdb_id):
+    print "\n\n\nin getTMDBID()"
+    url_rq = urlTMDB % ('find', imdb_id)
+    rq_movie = requests.get(url_rq, params_TMDB_find)
+    data_TMDB = json.loads(rq_movie.content)
+    print type(data_TMDB)
+    print type(data_TMDB['movie_results'])
+    print type(data_TMDB['movie_results'][0])
+    print data_TMDB['movie_results']
+    print rq_movie.url
+    print data_TMDB['movie_results'][0]['id']
+    print "\n\n movie_results"
+    print data_TMDB['movie_results']
+    return data_TMDB['movie_results'][0]['id']
+
+
+urlOMDB = "https://www.omdbapi.com/?%s"
+urlTMDB = "https://api.themoviedb.org/3/%s/%s"
+urlTMDBTrailer = "http://api.themoviedb.org/3/movie/%s/videos"
+urlYoutube = "https://www.youtube.com/watch?v=%s"
+params_TMDB_find = {'api_key': '7f276b4eaa72a85490d81dfb4504a1e6',
+                    'external_source': 'imdb_id'}
+params_TMDB_trailer = {'api_key': '7f276b4eaa72a85490d81dfb4504a1e6'}
 
 # Creating movies
-title_movies = ["Django", "Toy Story", "Avatar", "School of Rock",
-                "Ratatouille", "Midnight in Paris", "Django"]
+title_movies = ["Django Unchained", "Toy Story", "Avatar", "School of Rock",
+                "Ratatouille", "Midnight in Paris", "Toy Story 2",
+                "The Cove", "The Act of Killing"]
 movies = []
 
 for i in range(len(title_movies)):
     search_by_title = {'t': title_movies[i]}
-    response = requests.get(urlApiT, search_by_title)
+    response = requests.get(urlOMDB, search_by_title)
     jData = json.loads(response.content)
+    print "\n\n\n in for loop"
+    print jData
+    imdb_id = jData['imdbID']
+    trailer = getTrailer(getTMDBID(imdb_id))
     movies.append(media.Movie(jData['Title'], jData['Plot'],
-                              jData['Poster'], ""))
+                              jData['Poster'], trailer))
 
 """
 django = media.Movie(jData['Title'], jData['Plot'], jData['Poster'], "")
